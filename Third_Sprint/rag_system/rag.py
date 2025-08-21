8865846e6d3b4abd5f24831ad518649243c230fd
Steven-Ak <stevenakram2@gmail.com>
2025-08-21 15:06:05 +0300

    update rag

--- Third_Sprint/rag_system/rag.py
+++ Third_Sprint/rag_system/rag.py
@@ -5,38 +5,35 @@ from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from transformers import pipeline
 
-
 # Load & Split PDF
 def load_documents(pdf_path):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=350, chunk_overlap=80)
     return splitter.split_documents(docs)
 
-
 # Create Vector DB
-def create_vector_db(chunks, persist_path="db"):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+def create_vector_db(chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
     db = FAISS.from_documents(chunks, embeddings)
     return db
-
 
 # Setup Generator Model
 def get_llm():
     gen_pipeline = pipeline(
         "text2text-generation",
-        model="google/flan-t5-small",
-        device=-1                      
+        model="google/flan-t5-base",
+        device=0,                
+        max_new_tokens=256       
     )
     return HuggingFacePipeline(pipeline=gen_pipeline)
-
 
 # Build RAG chain
 def build_qa_chain(db):
     llm = get_llm()
     qa = RetrievalQA.from_chain_type(
         llm=llm,
-        retriever=db.as_retriever(),
+        retriever=db.as_retriever(search_kwargs={"k": 4}),
         chain_type="stuff"
     )
     return qa
